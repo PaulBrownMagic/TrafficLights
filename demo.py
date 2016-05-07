aa8932dd3dfe050d3d50ffb59de0e97cf2b668fa
PaulBrownMagic <paul@paulbrownmagic.com>
2016-05-07 23:53:48 +0100

    Added descriptive comments

--- demo.py
+++ demo.py
@@ -8,27 +8,27 @@ while True:
     # red ON
     # amber OFF
     # green OFF
-    output(red, 1)
-    output(amber, 0)  # included here because of loop
+    output(red, True)
+    output(amber, False)  # included here because of loop
     sleep(2)
 
     # red ON
     # amber ON
     # green OFF
-    output(amber, 1)
+    output(amber, True)
     sleep(1)
 
     # red OFF
     # amber OFF
     # green ON
-    output(red, 0)
-    output(amber, 0)
-    output(green, 1)
+    output(red, False)
+    output(amber, False)
+    output(green, True)
     sleep(2)
 
     # red OFF
     # amber ON
     # green OFF
-    output(green, 0)
-    output(amber, 1)
+    output(green, False)
+    output(amber, True)
     sleep(1)
