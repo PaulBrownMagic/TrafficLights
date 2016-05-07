66266f55abad08f9c1bf07335ec80115771fbda4
PaulBrownMagic <paul@paulbrownmagic.com>
2016-05-07 23:52:42 +0100

    Added descriptive comments

--- demo.py
+++ demo.py
@@ -5,15 +5,30 @@ amber = 'amber'
 green = 'green'
 
 while True:
+    # red ON
+    # amber OFF
+    # green OFF
     output(red, 1)
+    output(amber, 0)  # included here because of loop
     sleep(2)
+
+    # red ON
+    # amber ON
+    # green OFF
     output(amber, 1)
     sleep(1)
+
+    # red OFF
+    # amber OFF
+    # green ON
     output(red, 0)
     output(amber, 0)
     output(green, 1)
     sleep(2)
+
+    # red OFF
+    # amber ON
+    # green OFF
     output(green, 0)
     output(amber, 1)
     sleep(1)
-    output(amber, 0)
